e7ee570e15325d77b50f132345651ab96d9efc4b
Andrej Romanov <romanov454545@gmail.com>
2024-11-26 17:13:39 +0200

    1

---
+++ src/main/java/com/example/objektinis/ObjektinisApplication.java
@@ -0,0 +1,13 @@
+package com.example.objektinis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjektinisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ObjektinisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/objektinis/ObjektinisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.objektinis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObjektinisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
